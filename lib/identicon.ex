7521abb660481758703bb217daf56c80165dda26
zyadelhady <zyade40@gmail.com>
2023-11-06 22:18:38 +0200

    init the project

---
+++ lib/identicon.ex
@@ -0,0 +1,19 @@
+defmodule Identicon do
+  def main(input) do
+    input
+    |> has_input
+    |> pick_color
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def has_input(input) do
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
+
+    %Identicon.Image{hex: hex}
+  end
+end
